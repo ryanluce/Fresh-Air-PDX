96de780e04c0f455181f20b998818635f27ec308
Ryan Luce <ryan@ryanluce.com>
2014-10-22 18:52:19 -0700

    Initial Commit

---
+++ Fresh Air PDX/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fresh Air PDX
+//
+//  Created by Ryan Luce on 10/22/14.
+//  Copyright (c) 2014 Ryan Luce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Fresh Air PDX/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Fresh Air PDX
+//
+//  Created by Ryan Luce on 10/22/14.
+//  Copyright (c) 2014 Ryan Luce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
